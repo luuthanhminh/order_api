e46dfbad888c76397b908071e30dd06b05c183ba
Minh <minhltuit@gmail.com>
2019-12-05 12:37:49 +0700

    - Update swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './middlewares/all-exceptions.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  const options = new DocumentBuilder()
+  .setTitle('Order')
+  .setDescription('The order API description')
+  .setVersion('1.0')
+  .addTag('order')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3006);
 }
